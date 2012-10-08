3fcadd3cc45d5aa8f8a13c0c459394f3380e032e
orieken <oriekenjr@gmail.com>
2012-10-08 12:46:09 -0400

    added some more tests for the other needed elements

--- spec/isolation/capybara_basic_spec.rb
+++ spec/isolation/capybara_basic_spec.rb
@@ -8,5 +8,9 @@ describe "Capybara examples" do
   end
 
   it { page.should have_content(Time.now.strftime('%D')) }
+  it { page.should have_selector('form') }
   it { page.should have_selector('h1') }
+  it { page.should have_selector('input#start_date') }
+  it { page.should have_selector('input#end_date') }
+  it { page.should have_button('Submit') }
 end
