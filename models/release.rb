e2813032191f94d97f400833baf1502730812df3
orieken <oriekenjr@gmail.com>
2012-10-10 14:21:57 -0400

    remembered to add the models folder and that i added a factory for testing

---
+++ models/release.rb
@@ -0,0 +1,11 @@
+class Release < ActiveRecord::Base
+  include ActiveModel::Validations
+  attr_accessor :release_number, :release_date
+  validates_presence_of :release_number, :release_date
+  validates_numericality_of :release_number
+
+
+  def self.releases_in_range(start_date, end_date)
+    Release.find(:all, :conditions => {:release_date => (start_date)...(end_date)})
+  end
+end
